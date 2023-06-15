bed306d037b78213607222e90610204dac0bc471
Ivan Melentyev <ivan.saxalin@mail.ru>
2023-06-15 12:23:58 +0300

    Обновлять url в строке состояния браузера

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,11 @@
 import ACommands from "../pages/ACommands/ACommands.vue";
 import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 import ACommand from "../pages/ACommand/ACommand.vue";
-import { computed } from "vue";
 import AAbout from "../pages/AAbout/AAbout.vue";
 import ARoles from "../pages/ARoles/ARoles.vue";
 import ADon from "../pages/ADon/ADon.vue";
 import AEvents from "../pages/AEvents/AEvents.vue";
+import bridge from "@vkontakte/vk-bridge";
 
 const routes: RouteRecordRaw[] = [
   { path: "/", component: ACommands },
@@ -27,13 +27,19 @@ export const router = createRouter({
   routes,
 });
 
-export const routerBackExist = computed(() => true);
-
-export const routerBack = () => {
-  if (routerBackExist.value) {
-    router.back();
-    return true;
+router.beforeEach(async (to, from) => {
+  if (to.query?.vk_app_id && from.fullPath === "/") {
+    return { path: to.hash.replace("#", "") || "/", replace: true };
   }
-
-  return false;
-};
+});
+router.afterEach((to) => {
+  bridge.send("VKWebAppSetLocation", {
+    location: to.fullPath,
+    replace_state: true,
+  });
+});
+bridge.subscribe((event) => {
+  if (event.detail.type === "VKWebAppChangeFragment") {
+    router.replace(event.detail.data.location);
+  }
+});
