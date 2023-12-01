2855debacd1efcd9bd085c2dafb1d7e66a487556
Ivan Melentyev <ivan.saxalin@mail.ru>
2023-12-01 04:01:34 +0300

    feat: инструкция по попаданию в каталог

--- src/pages/ATops/ATopConversationsCatalog.vue
+++ src/pages/ATops/ATopConversationsCatalog.vue
@@ -2,6 +2,7 @@
 import { useAppCaption } from "../../hooks/useAppCaption";
 import ATopConversationsTable from "./ATopConversationsTable.vue";
 import APageContainer from "../../components/APageContainer/APageContainer.vue";
+import AButton from "../../components/AButton/AButton.vue";
 
 useAppCaption("Каталог чатов");
 const items: object[] = await fetch(
@@ -10,6 +11,28 @@ const items: object[] = await fetch(
 </script>
 <template>
   <APageContainer style="padding-inline: 0">
+    <details style="padding: var(--page-padding)">
+      <summary style="cursor: pointer">Хотите попасть в каталог?</summary>
+      <ol>
+        <li>
+          1) Установите ссылку на ваш чат с помощью команды:
+          <AButton class="a-button__inline" to="/command/63">
+            ЗадатьСсылку
+          </AButton>
+        </li>
+        <li>
+          2) Выполните команду:
+          <AButton class="a-button__inline" to="/command/70">
+            ПубличнаяСсылка
+          </AButton>
+        </li>
+      </ol>
+      Для скрытия ссылки выполните команду
+      <AButton class="a-button__inline" to="/command/70">
+        ПубличнаяСсылка
+      </AButton>
+      ещё раз.
+    </details>
     <ATopConversationsTable :items="items" />
   </APageContainer>
 </template>
